b77abf323dbb8580d8f1c399611cc4644e883859
nodeshells <kenta.job09@gmail.com>
2018-01-30 13:20:10 +0900

    update

--- src/app/review/detail/detail.component.ts
+++ src/app/review/detail/detail.component.ts
@@ -9,13 +9,19 @@ import { Http } from '@angular/http';
   styleUrls: ['./detail.component.css']
 })
 export class DetailComponent {
-  items;
+  items = {};
   reviewid;
   avatar = './assets/user1/user1_profile.jpg';
   queryParams: any;
   constructor (private _activatedRoute: ActivatedRoute,
     private _router: Router,
     private http: Http) {
+    this._activatedRoute.queryParams.subscribe(
+        params => {
+          this.queryParams = params;
+          this.reviewid = this.queryParams.id;
+        });
+    this.onLoad();
   }
 
   ngOnInit () {
@@ -23,8 +29,8 @@ export class DetailComponent {
         params => {
           this.queryParams = params;
           this.reviewid = this.queryParams.id;
-          this.onLoad();
         });
+    this.onLoad();
   }
 
   onLoad () {
@@ -32,7 +38,6 @@ export class DetailComponent {
     .subscribe(
       response => {
         this.items = response.json();
-        console.log(this.items);
         // レビューの一覧を取得して最新順にしてある。
       },
       error => {
