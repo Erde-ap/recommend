d7778e08724b9cad5499e44e27d26ea210c65ccb
nodeshells <kenta.job09@gmail.com>
2018-02-05 10:37:39 +0900

    リダイレクトコンフィグにデータを追加

--- server/redirect_config.ts
+++ server/redirect_config.ts
@@ -7,5 +7,11 @@ export const LOGIN_F_REDIRECT_URL = 'http://localhost:4200/register';
 // メール認証終了後(成功)のリダイレクト先(あとで変更)
 export const SEND_REDIRECT_URL = 'http://localhost:4200';
 
+// // メール認証終了後(成功)のリダイレクト先(あとで変更)
+// export const SEND_REDIRECT_URL = 'https://reco.site';
+
 // メール認証終了後（失敗）のリダイレクト先(あとで変更)
 export const FAIL_REDIRECT_URL = 'http://localhost:4200/register';
+
+// // メール認証終了後（失敗）のリダイレクト先(あとで変更)
+// export const FAIL_REDIRECT_URL = 'http://reco.site/register';
